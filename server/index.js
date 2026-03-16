d65fe0b550d052252dc63690f7fc736cce38f3dc
KG <kgcodes@pm.me>
2026-03-15 21:40:05 -0700

    fix: updated cohost section in my accounts

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ app.use(express.json());
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/listings', require('./routes/listings'));
+app.use('/api/listings', require('./routes/cohosts')); 
 app.use('/api/rooms', require('./routes/rooms'));
 app.use('/api/jobs', require('./routes/jobs'));
 app.use('/api/users', require('./routes/users'));
